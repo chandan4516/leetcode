d3f77d7f017e559d8f11e93ed18517aaf1ed6c39
Chandan Chaman <chandanchaman@Chandans-MacBook-Air.local>
2025-11-05 23:57:12 -0800

    206. Reverse Linked List - https://leetcode.com/problems/reverse-linked-list/description/

---
+++ src/com/leetcode/ReverseLinkedList206.java
@@ -0,0 +1,50 @@
+/**
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [1,2,3,4,5]
+ * Output: [5,4,3,2,1]
+ * Example 2:
+ *
+ *
+ * Input: head = [1,2]
+ * Output: [2,1]
+ * Example 3:
+ *
+ * Input: head = []
+ * Output: []
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the list is the range [0, 5000].
+ * -5000 <= Node.val <= 5000
+ */
+package com.leetcode;
+
+// Definition for singly-linked list.
+ class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+
+public class ReverseLinkedList206 {
+    public ListNode reverseList(ListNode head) {
+        ListNode current = head;
+        ListNode prev = null;
+        while(current != null){
+            ListNode temp = current.next;
+            current.next = prev;
+            prev = current;
+            current = temp;
+        }
+        return prev;
+    }
+}
