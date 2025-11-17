f1b5f8c845d591b71f571a677bd81c0b6aa06718
Chandan Chaman <chandanchaman@Chandans-MacBook-Air.local>
2025-11-17 12:40:14 -0800

    110. Balanced Binary Tree - https://leetcode.com/problems/balanced-binary-tree/description/

--- src/com/leetcode/BalancedBinaryTree110.java
+++ src/com/leetcode/BalancedBinaryTree110.java
@@ -1,2 +1,48 @@
-package com.leetcode;public class BalancedBinaryTree110 {
+package com.leetcode;
+
+/**
+ * Given a binary tree, determine if it is height-balanced.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: true
+ * Example 2:
+ *
+ *
+ * Input: root = [1,2,2,3,3,null,null,4,4]
+ * Output: false
+ * Example 3:
+ *
+ * Input: root = []
+ * Output: true
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the tree is in the range [0, 5000].
+ * -104 <= Node.val <= 104
+ */
+
+public class BalancedBinaryTree110 {
+    public boolean isBalanced(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+        if(Math.abs(leftHeight-rightHeight) >=2){
+            return false;
+        }
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+    public int getHeight(TreeNode node){
+        if(node==null){
+            return 0;
+        }
+        return 1+Math.max(getHeight(node.left), getHeight(node.right));
+    }
 }
